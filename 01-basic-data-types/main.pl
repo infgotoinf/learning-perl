f37cc78f703fe8c2156a4cf286c9487207543eb2
-inf <infgotoinf@gmail.com>
2026-03-16 22:35:29 +0300

    01-basic-data-types: Write about operators, make print pretier and code more readable

--- 01-basic-data-types/main.pl
+++ 01-basic-data-types/main.pl
@@ -12,7 +12,7 @@ my $var;
 
 
 
-# Scalars
+print "Scalars\n";
 # https://stackoverflow.com/questions/67292717/what-does-scalar-mean-in-perl
 
 # In perl there sre 3 main types:
@@ -28,30 +28,47 @@ my %c = (          # Hashes (associative arrays of scalars (just like maps))
 # not with that we operate
 print $a          ." - Single variable is scalar\n";
 print $b[0]       ." - Array [0] element is also scalar\n";
-print $c{'first'} ." - Hash {'first'} element is also scalar\n\n";
+print $c{'first'} ." - Hash {'first'} element is also scalar\n";
 
 
 
-# Numbers
+print "\nNumbers\n";
 # https://www.perltutorial.org/perl-numbers/
 my $n = 123_456_789; # You can add _ in numbers so they're easier to read
-print $n, "\n";
+print "123_456_789 = ", $n, "\n-\n";
 
 # Integers
 print 124, "\n";
 print -34, "\n";
-print 0b1100, "\n"; # Binary
-print 0255, "\n";   # Octal
-print 0xABC, "\n";  # Hexidecimal
+print "0b1100 = ", 0b1100, "\n";   # Binary
+print "0255   = ", 0225, "\n";     # Octal
+print "0xABC  = ", 0xABC, "\n-\n"; # Hexidecimal
 
 # Floats
 print 228.69, "\n";
-print +1.023e2,"\n";
-print -1.023E2,"\n\n";
+print "+1.023e2 = ", +1.023e2,"\n";
+print "-1.023E2 = ", -1.023E2,"\n";
 
 
 
-# Strings
+print "\nOperators\n";
+# https://www.perltutorial.org/perl-operators/
+# Basic operators: + - / * %
+print "5**2 = ", 5**2, "\n"; # Power of a number
+
+# Bitwise operators:
+# and &, or |, exclusive or ^ , not ~, shift right >>, shift left <<
+
+# Comparison operators:
+# == != < > <= >=
+# Spaceship operator <=>
+print "10 <=> 20 = ", 10 <=> 20, "\n";
+print "10 <=> 10 = ", 10 <=> 10, "\n";
+print "10 <=> 5  = ", 10 <=> 5, "\n";
+
+
+
+print "\nStrings\n";
 # https://www.perltutorial.org/perl-string/
 
 # Can be single and double-quoted
@@ -62,15 +79,16 @@ my $s2 = 'also string';
 my $s3 = q/string "string" string/;
 my $s4 = qq/string 'string' string/;
 print $s3, "\n";
-print $s4, "\n";
+print $s4, "\n-\n";
 
 # but / can be replaced with any other special symbol
 my $s5 = q^string STRING string^;
 
-# String funtions
-print length($s5), "\n"; # Returns number of characters in a string
-print uc($s5), "\n";     # Returns in uper case
-print lc($s5), "\n";     # Returns in lower case
+# String functions
+print $s5         ." - original string\n";
+print length($s5) ." - lenght()\n";
+print uc($s5)     ." - uc()\n";
+print lc($s5)     ." - lc()\n";
 
 # Extracts substring from a string
 print substr($s5, 7, 6), "\n";
