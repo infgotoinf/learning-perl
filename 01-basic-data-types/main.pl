e152a6954ab1e8e2173592158576d8bcd075405d
-inf <infgotoinf@gmail.com>
2026-03-16 23:13:33 +0300

    Create 02-basic-data-structures/main.pl and move here section about scalars from 01-basic-data-types/main.pl

--- 01-basic-data-types/main.pl
+++ 01-basic-data-types/main.pl
@@ -12,26 +12,6 @@ my $var;
 
 
 
-print "Scalars\n";
-# https://stackoverflow.com/questions/67292717/what-does-scalar-mean-in-perl
-
-# In perl there sre 3 main types:
-my $a = 1;         # Scalars (singele valued variables)
-my @b = (1, 2, 3); # Arrays (arrays of scalars)
-my %c = (          # Hashes (associative arrays of scalars (just like maps))
-    first => 1,
-    second => 2,
-    third => 3,
-);
-
-# Special symbols ($, @, %) represent what we want to get,
-# not with that we operate
-print $a          ." - Single variable is scalar\n";
-print $b[0]       ." - Array [0] element is also scalar\n";
-print $c{'first'} ." - Hash {'first'} element is also scalar\n";
-
-
-
 print "\nNumbers\n";
 # https://www.perltutorial.org/perl-numbers/
 my $n = 123_456_789; # You can add _ in numbers so they're easier to read
