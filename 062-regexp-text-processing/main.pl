0598a13a60b892102e39d91649324f47a9a2e00d
-inf <infgotoinf@gmail.com>
2026-03-25 21:17:26 +0300

    062-regexp: Continue reading, learn about \U \L \Q \E \l \u, /g and /m for s///

--- 062-regexp-text-processing/main.pl
+++ 062-regexp-text-processing/main.pl
@@ -35,9 +35,83 @@ print "$_\n";  # "cave, sweet cave!"
 # you can do this like that
 my $original = 'Fred ate 1 rib';
 (my $copy = $original) =~ s/\d+ ribs?/10 ribs/;
+say $original;
 
 # You can also use /r flag that makes so s/// returns modifyed copy of the string,
 # not the number of substitutions made
-my $copy = $original =~ s/\d+ ribs?/10 ribs/r;
+$copy = $original =~ s/\d+ ribs?/10 ribs/r;
+say $original;
 # The difference here from the previous one is that we first do the substitution
 # and then the assignment
+
+
+# \U make all words upercased
+# \L lowercased
+# \E to end formatting
+$_ = "I saw barney with fred.";
+s/(\w+) with (\w+)/\U$2\E with $1/i;  # $_ is now "I saw FRED with barney."
+say $_;
+
+# \u to make only next character upercased
+# \l lowercased
+s/(fred|barney)/\u$1/ig;  # $_ is now "I saw FRED with Barney."
+say $_;
+
+# \u\L will make all words lowercased with uppercased first letter
+s/(fred|barney)/\u\L$1/ig;  # $_ is now "I saw Fred with Barney."
+say $_;
+
+# \Q will quote all meta characters after it
+if ( s/\Q(((\E(Fred)/$1/ ) {   # Even less messy
+  print "Cleansed $1\n";
+}
+
+# split() creates an array by sperating
+my @fields = split /:/, "abc:def:g:h";  # gives ("abc", "def", "g", "h")
+
+# -1 as a 3rd argument saves trailing spaces at the end
+@fields = split /:/, ":::a:b:c:::";  # gives ("", "", "", "a", "b", "c")
+@fields = split /:/, ":::a:b:c:::", -1;  # gives
+# ("", "", "", "a", "b", "c", "", "", "")
+
+# Join joins arrays in one string separated by a separator
+my $x = join ":", 4, 6, 8, 10, 12;  # $x is "4:6:8:10:12
+say $x;
+
+# TODO: Understand (195 page)
+my $text = "Fred dropped a 5 ton granite block on Mr. Slate";
+my @words = ($text =~ /([a-z]+)/ig);
+print "Result: @words\n"; # Result: Fred dropped a ton granite block on Mr Slat
+
+# By default .* is greedy that means it takes as much as it can
+# you can use .*? to make it nongreedy
+$text = '<b>Fred</b> and <b>Barney</b>';
+my $match_count = $text =~ s|<b>(.*)</b>|\U$1|g;
+print "$match_count: $text\n";
+
+$text = '<b>Fred</b> and <b>Barney</b>';
+$match_count = $text =~ s|<b>(.*?)</b>|\U$1|g; # nongreedy
+print "$match_count: $text\n";
+
+# ??     Least of zero or one match
+# *?     Zero or more, as few as possible
+# +?     One or more, as few as possible
+# {3,}?  At least three, but as few as possible
+# {3,5}? At least three, as many as fve, but as few as possible
+
+# By default in regexp boundary \b works wierd
+my $string = "This doesn't capitalize correctly.";
+$string =~ s/\b(\w)/\U$1/g;
+print "$string\n";
+
+# You can make boundary smarter by using \b{wb} (word boundary)
+$string = "This doesn't capitalize correctly.";
+$string =~ s/\b{wb}(\w)/\U$1/g;
+print "$string\n";
+
+# \b{sb} that is sentence boundary, uses a set of rules to guess if punctuation
+# is at the end of a sentence or is it a punctuation like in Mr. Flintstone
+# \b{lb} indicates a good place to break text, so you don't wrap line in a middle of a word,
+# it knows there to insert newlines
+
+# //m makes multiline so ^ $ apply to each line
